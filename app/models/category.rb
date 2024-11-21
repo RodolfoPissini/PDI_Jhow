e293dae4f832fe071c312413e2382a206fac0fca
RodolfoPissini <rodolfo.pissini@outlook.com>
2024-11-21 10:37:17 -0300

    treinamento 2

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,12 @@
 class Category < ApplicationRecord
+  validates :name, presence: true
+  validates :slug, uniqueness: true, allow_nil: true
+
+  before_save :generate_slug
+
+  private
+
+  def generate_slug
+    self.slug = name.parameterize if name.present? && slug.blank?
+  end
 end
