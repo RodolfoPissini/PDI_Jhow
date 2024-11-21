95fe6c05eafdd4665dd1715fdc6fa8ba72bc6299
RodolfoPissini <rodolfo.pissini@outlook.com>
2024-11-21 11:56:13 -0300

    treinamento 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_21_131512) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_21_143603) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_21_131512) do
     t.string "status", default: "PENDING"
     t.string "image_url"
     t.string "slug"
+    t.integer "parent_id"
+    t.index ["parent_id"], name: "index_categories_on_parent_id"
   end
 
 end
